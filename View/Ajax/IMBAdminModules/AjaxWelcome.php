c871263a426f2c4b8d2a83d9875f3027a5f515b3
Marc Urben <aegnor@mittelerde.ch>
2011-05-10 00:59:50 +0200

    renamed the ImbaNavigation to ImbaContentNavigation

--- View/Ajax/IMBAdminModules/AjaxWelcome.php
+++ View/Ajax/IMBAdminModules/AjaxWelcome.php
@@ -9,7 +9,7 @@ class AjaxWelcome extends AjaxBase {
         parent::__construct();
     }
 
-    public function getNavigation() {
+    public function getContentManager() {
         /**
          * Define Navigation
          */
@@ -66,6 +66,112 @@ class AjaxWelcome extends AjaxBase {
             }
         }
 
+        /**
+         * new code
+         *
+          $myself = $managerUser->selectMyself();
+          $allUsers = $managerUser->selectAllUser();
+          $smarty->assign('nickname', $myself->getNickname());
+          $smarty->assign("today", date("d") . "." . date("m") . " " . date("Y"));
+          $smarty->assign("thrustRoot", urlencode(ImbaSharedFunctions::getSiteDomainUrl()));
+          /*
+         * ToDo:
+         * $events
+         * $todo
+         * $today
+         * $myName
+         * 
+         */
+        /*
+         * Fill Navigation $navs
+         *
+        $navOptions = array();
+        if ($handle = opendir('Ajax/IMBAdminModules/')) {
+            $identifiers = array();
+            while (false !== ($file = readdir($handle))) {
+                if (strrpos($file, ".Navigation.php") > 0) {
+                    include 'Ajax/IMBAdminModules/' . $file;
+                    if (ImbaUserContext::getUserRole() >= $Navigation->getMinUserRole()) {
+                        $showMe = false;
+                        if (ImbaUserContext::getLoggedIn() && $Navigation->getShowLoggedIn()) {
+                            $showMe = true;
+                        } elseif ((!ImbaUserContext::getLoggedIn()) && $Navigation->getShowLoggedOff()) {
+                            $showMe = true;
+                        }
+
+                        if ($showMe) {
+                            $modIdentifier = trim(str_replace(".Navigation.php", "", $file));
+                            array_push($navOptions, array("identifier" => $modIdentifier,
+                                "name" => $Navigation->getName($nav),
+                                "comment" => $Navigation->getComment($nav)
+                            ));
+                            $Navigation = null;
+                        }
+                    }
+                }
+            }
+            closedir($handle);
+        }
+        $smarty->assign('navs', $navOptions);
+
+        **
+         * Fill $birthdays
+         *
+        $return = "";
+        $birthdays = array();
+        $todayMagicNumber = (date("n") * 31) + date("j");
+        foreach ($allUsers as $user) {
+            $magicNumber = ($user->getBirthmonth() * 31) + $user->getBirthday();
+            $birthdayStr = $user->getNickname() . ": " . $user->getBirthday() . "." . $user->getBirthmonth() . " (" . (date("Y") - $user->getBirthyear()) . ")<br />";
+            if ($magicNumber > 0) {
+                $birthdays[$magicNumber] .= $birthdayStr;
+            }
+        }
+        $count = 0;
+        ksort($birthdays);
+        foreach ($birthdays as $birthday => $string) {
+            if ($birthday >= $todayMagicNumber) {
+                if ($todayMagicNumber == $birthday) {
+                    $return .= "<b>" . $string . "</b>";
+                } else {
+                    $return .= $string;
+                }
+                $count++;
+                if ($count > 2) {
+                    break;
+                }
+            }
+        }
+        $smarty->assign("birthdays", $return);
+
+        **
+         * Fill $newMembers
+         *
+        $return = "";
+        $newUsers = array();
+        foreach ($allUsers as $user) {
+            $newUsers[$user->getId()] = $user->getNickname();
+        }
+        krsort($newUsers);
+        $count = 0;
+        foreach ($newUsers as $id => $nickName) {
+            if ($count > 2) {
+                break;
+            } else {
+                $return .= $nickName . "<br />";
+                $count++;
+            }
+        }
+        $smarty->assign("newMembers", $return);
+
+        **
+         * Display the site
+         *
+        $smarty->display('IMBAdminModules/WelcomeOverview.tpl');
+        break;
+        *
+        *
+        */
         $this->smarty->assign('navs', $topNavigation);
         $this->smarty->display('IMBAdminModules/WelcomeOverview.tpl');
     }
