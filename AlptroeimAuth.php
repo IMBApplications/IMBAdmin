11aca31f15ccf0e7cc97e3ffc26a3311510721be
Aggravate <Steffen.So@gmail.com>
2011-05-30 09:39:38 +0200

    AlptroeimAuth.php only called once

--- AlptroeimAuth.php
+++ AlptroeimAuth.php
@@ -2,11 +2,16 @@
 
 session_start();
 
-$postvars = "secSession=" . session_id() . "&facility=me";
-session_write_close();
-$username = file_get_contents("http://localhost/IMBAdmin/ImbaProxy.php?" . $postvars);
+if ($_SESSION["AlptrLoggedIn"] == null) {
+    $postvars = "secSession=" . session_id() . "&facility=me";
+    session_write_close();
+    $username = file_get_contents("http://localhost/IMBAdmin/ImbaProxy.php?" . $postvars);
 
-include("/../wordpress/wp-load.php");
-wp_setcookie($username);
+    include("/../wordpress/wp-load.php");
+    wp_setcookie($username);
+}
+
+session_start();
+$_SESSION["AlptrLoggedIn"] = "1";
 echo "done";
 ?>
