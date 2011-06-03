08803045c4996622b4ac06580f03612a201a926b
Steffen Sommer <steffen.so@gmail.com>
2011-06-03 14:14:57 +0200

    working on openid login

--- Test/openid.php
+++ Test/openid.php
@@ -1,40 +1,12 @@
 <?php
-chdir("../");
-require_once ("Libs/lightopenid/LightOpenID.php");
+chdir("../Libs/");
+require_once ("Zend/OpenID.php");
+require_once ("Zend/OpenId/Consumer.php");
+require_once ("Zend/Controller/Exception.php");
 
-$lightOpenId = new LightOpenID();
-$lightOpenId->verify_peer = false;
-$lightOpenId->returnUrl = "http://dev.alptroeim.ch/IMBAdmin/Test/openid.php?step=2";
-$lightOpenId->realm = "http://dev.alptroeim.ch";
-
-if ($_GET["step"] == 1) {
-    if (!$lightOpenId->mode) {
-        $lightOpenId->identity = $_GET["openid"];
-        //$lightOpenId->identity = "https://oom.ch/openid/identity/sampit";
-        //$lightOpenId->identity = "http://openid-provider.appspot.com/Steffen.So@googlemail.com";
-        header('Location: ' . $lightOpenId->authUrl());
-    }
-} else if ($_GET["step"] == 2) {
-    try {
-        echo "GET: <br>\n";
-        foreach ($_GET as $key => $value) {
-            echo $key . " = " . rawurldecode($value) . "<br>\n";
-        }
-
-        echo $lightOpenId->validate() ? 'Logged in.' : 'Failed';
-    } catch (Exception $ex) {
-        echo "Fehler: <br>";
-        echo $ex->getMessage();
-    }
-} else {
-    ?>
-    <form method="get">
-        Sign in with your OpenID: <br/>
-        <input type="text" name="openid" size="30" />
-        <input type="hidden" name="step" value="1" />
-        <br />
-        <input type="submit" name="submit" value="Log In" />
-    </form>
-    <?php
+$consumer = new Zend_OpenId_Consumer();
+if ($consumer->login("http://openid-provider.appspot.com/Steffen.So@googlemail.com") === false)
+{
+    echo "failed";
 }
 ?>
