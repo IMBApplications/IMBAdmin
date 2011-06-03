11e46aa620e8a338bd13e35939b4039399b1f3cf
Steffen Sommer <steffen.so@gmail.com>
2011-06-03 14:48:13 +0200

    working on openid login

--- Test/testlogin.php
+++ Test/testlogin.php
@@ -1,32 +1,13 @@
 <?php
+
 chdir("../Libs/");
 require_once ("Zend/OpenID.php");
 require_once ("Zend/OpenId/Consumer.php");
 require_once ("Zend/Controller/Exception.php");
 
-$status = "Start";
-
-try {
-    if (!isset($_GET['openid_mode'])) {
-        $consumer = new Zend_OpenId_Consumer();
-        if (!$consumer->login("http://openid-provider.appspot.com/Steffen.So@googlemail.com")) {
-            $status = "OpenID login failed.";
-        }
-    } else if (isset($_GET['openid_mode'])) {
-        if ($_GET['openid_mode'] == "id_res") {
-            $consumer = new Zend_OpenId_Consumer();
-            if ($consumer->verify($_GET, $id)) {
-                $status = "VALID " . htmlspecialchars($id);
-            } else {
-                $status = "INVALID " . htmlspecialchars($id);
-            }
-        } else if ($_GET['openid_mode'] == "cancel") {
-            $status = "CANCELLED";
-        }
-    }
-} catch (Exception $ex) {
-    echo "Exception: " . $ex->getMessage();
+$consumer = new Zend_OpenId_Consumer();
+if (!$consumer->login("http://openid-provider.appspot.com/Steffen.So@googlemail.com")) {
+    echo "OpenID login failed.";
+} else {
+    echo "Logged in";
 }
-
-echo $status;
-?>
