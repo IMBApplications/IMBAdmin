41def512c4a95754ea6440fba19a681d3551769f
Steffen Sommer <steffen.so@gmail.com>
2011-06-03 14:30:37 +0200

    working on openid login

--- Test/testlogin.php
+++ Test/testlogin.php
@@ -3,18 +3,13 @@ chdir("../Libs/");
 require_once ("Zend/OpenID.php");
 require_once ("Zend/OpenId/Consumer.php");
 require_once ("Zend/Controller/Exception.php");
-?>
 
-<?php
+$status = "Start";
+
 try {
-
-    $status = "";
-    if (isset($_POST['openid_action']) &&
-            $_POST['openid_action'] == "login" &&
-            !empty($_POST['openid_identifier'])) {
-
+    if (!isset($_GET['openid_mode'])) {
         $consumer = new Zend_OpenId_Consumer();
-        if (!$consumer->login($_POST['openid_identifier'])) {
+        if (!$consumer->login("http://openid-provider.appspot.com/Steffen.So@googlemail.com")) {
             $status = "OpenID login failed.";
         }
     } else if (isset($_GET['openid_mode'])) {
@@ -32,16 +27,6 @@ try {
 } catch (Exception $ex) {
     echo "Exception: " . $ex->getMessage();
 }
+
+echo $status;
 ?>
-<html>
-    <body>
-<?php echo "$status<br>" ?>
-        <form method="post">
-            <fieldset>
-                <legend>OpenID Login</legend>
-                <input type="text" name="openid_identifier" value=""/>
-                <input type="submit" name="openid_action" value="login"/>
-            </fieldset>
-        </form>
-    </body>
-</html>
