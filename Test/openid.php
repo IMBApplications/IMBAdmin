caad6fcd75af3ff408a3a5f04226994b97ad6154
Steffen Sommer <steffen.so@gmail.com>
2011-06-03 12:36:28 +0200

    working on openid login

--- Test/openid.php
+++ Test/openid.php
@@ -1,5 +1,4 @@
 <?php
-
 chdir("../");
 require_once ("Libs/lightopenid/LightOpenID.php");
 
@@ -8,16 +7,17 @@ $lightOpenId->verify_peer = false;
 $lightOpenId->returnUrl = "http://dev.alptroeim.ch/IMBAdmin/Test/openid.php?step=2";
 $lightOpenId->realm = "http://dev.alptroeim.ch";
 
-if (empty($_GET["step"])) {
+if ($_GET["step"] == 1) {
     if (!$lightOpenId->mode) {
-        $lightOpenId->identity = "https://oom.ch/openid/identity/sampit";
+        $lightOpenId->identity = $_GET["openid"];
+        //$lightOpenId->identity = "https://oom.ch/openid/identity/sampit";
         //$lightOpenId->identity = "http://openid-provider.appspot.com/Steffen.So@googlemail.com";
         header('Location: ' . $lightOpenId->authUrl());
     }
 } else if ($_GET["step"] == 2) {
     try {
         echo "GET: <br>\n";
-        foreach ($_GET as $key=>$value) {
+        foreach ($_GET as $key => $value) {
             echo $key . " = " . rawurldecode($value) . "<br>\n";
         }
 
@@ -26,5 +26,15 @@ if (empty($_GET["step"])) {
         echo "Fehler: <br>";
         echo $ex->getMessage();
     }
+} else {
+    ?>
+    <form method="get">
+        Sign in with your OpenID: <br/>
+        <input type="text" name="openid" size="30" />
+        <input type="hidden" name="step" value="1" />
+        <br />
+        <input type="submit" name="submit" value="Log In" />
+    </form>
+    <?php
 }
 ?>
