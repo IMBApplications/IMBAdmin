865de95d4620f8457ac2f2008ff2f3362aa8fcaf
Steffen Sommer <steffen.so@gmail.com>
2011-06-03 15:05:55 +0200

    working on openid login

--- Test/testlogin.php
+++ Test/testlogin.php
@@ -1,13 +1,13 @@
 <?php
 
-echo "Immernoch nicht einverstanden? 1.1";
+echo "Immernoch nicht einverstanden? 1.2";
 
 chdir("../Libs/");
 require_once ("Zend/OpenID.php");
 require_once ("Zend/OpenId/Consumer.php");
-/*
 require_once ("Zend/Controller/Exception.php");
 
+/*
 
 $consumer = new Zend_OpenId_Consumer();
 if (!$consumer->login("http://openid-provider.appspot.com/Steffen.So@googlemail.com")) {
