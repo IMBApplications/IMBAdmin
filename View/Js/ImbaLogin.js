f6c00e92f77560dd06abd523d6c5649ab4b8688c
Marc Urben <aegnor@mittelerde.ch>
2011-07-26 18:11:58 +0200

    portal title corrected

--- View/Js/ImbaLogin.js
+++ View/Js/ImbaLogin.js
@@ -323,7 +323,7 @@ function loadImbaPortal(portalId) {
     }, function (response){
         if (checkReturn(response) == false) {
             var currentPortal = $.parseJSON(response);
-            document.name = currentPortal.titlename;
+            document.title = currentPortal.titlename;
 
             if ((portalId != null) && (portalId != -1)) {
                 $.jGrowl('<img src="' + currentPortal.icon + 
